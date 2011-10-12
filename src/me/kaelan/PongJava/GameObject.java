0acc24921a62c19428e74cc1ec0276d720684c9c
Kaelan <eternity01033@hotmail.com>
2011-10-12 13:26:36 +0800

    GameObject class complete, rendering needs help

--- src/me/kaelan/PongJava/GameObject.java
+++ src/me/kaelan/PongJava/GameObject.java
@@ -1,5 +1,7 @@
 package me.kaelan.PongJava;
 
+import java.awt.*;
+
 public class GameObject {
 private int height;
 private int width;
@@ -44,8 +46,33 @@ public void setX(int nx)
 {
 	x = nx;
 }
-public void scale (int windowwidth, int windowheight)
+public void scale (int windowwidth, int windowheight, GameObject me)
 {
+	float scalenum;
+	if (windowwidth>windowheight)
+	{
+		scalenum = windowwidth/windowheight;
+		me.width = (int) (me.width * scalenum);
+		me.height = (int) (me.height * scalenum);
+	}
+	else if (windowwidth==windowheight)
+	{
+		scalenum = windowheight/windowwidth;
+		me.width = (int) (me.width * scalenum);
+		me.height = (int) (me.height * scalenum);
+	}
+	else
+	{
+		scalenum = windowheight/windowwidth;
+		me.width = (int) (me.width * scalenum);
+		me.height = (int) (me.height * scalenum);
+	}
 	
 }
+public void render(GameObject object, Color colour, Graphics g)
+{
+	g.setColor(colour);
+	g.drawRect(object.x, object.y, object.width, object.height);
+}
+
 }
--- src/me/kaelan/PongJava/PongJava.java
+++ src/me/kaelan/PongJava/PongJava.java
@@ -1,5 +1,13 @@
 package me.kaelan.PongJava;
-
-public class PongJava {
-
+import java.applet.Applet;
+import java.awt.*;
+public class PongJava extends Applet{
+	public GameObject paddle1;
+	public void init(){
+		paddle1 = new GameObject(0,0,40,100);
+	}
+	public void paint (Graphics g)
+	{
+        paddle1.render(paddle1, Color.yellow,g);
+	}
 }
